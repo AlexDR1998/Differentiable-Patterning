981da04aec91e3b03932ebe53d528f27bec4bac4
Alex <alexander.richardson@gmail.com>
2024-09-23 12:00:43 +0100

    fixed time sampling, varying noise proportion

--- Common/eddie_indexer.py
+++ Common/eddie_indexer.py
@@ -349,7 +349,7 @@ def index_to_pde_gray_scott_hyperparameters(index):
 
 
 def index_to_pde_gray_scott_rda(index):
-	indices = np.unravel_index(index,(8))
+	indices = np.unravel_index(index,(12))
 	params = {
 		"LOSS_FUNCTION":[euclidean,spectral_weighted][0],
 		"OPTIMISER":[optax.nadam,optax.nadamw][1],
