05c08474424a45fdef0b5ba4dd354e36e8fd31e5
Alex <alexander.richardson@gmail.com>
2025-01-13 16:49:27 +0000

    updated gated NCA model

--- NCA/model/NCA_gated_model.py
+++ NCA/model/NCA_gated_model.py
@@ -14,7 +14,14 @@ class gNCA(NCA):
     FIRE_RATE: float
     op: Ops
     perception: callable
-    def __init__(self, N_CHANNELS, KERNEL_STR=["ID","LAP"], ACTIVATION=jax.nn.relu, PADDING="CIRCULAR", FIRE_RATE=1.0, KERNEL_SCALE = 1, key=jax.random.PRNGKey(int(time.time()))):
+    def __init__(self,
+                N_CHANNELS,
+                KERNEL_STR=["ID","LAP"], 
+                ACTIVATION=jax.nn.relu, 
+                PADDING="CIRCULAR", 
+                FIRE_RATE=1.0, 
+                KERNEL_SCALE = 1, 
+                key=jax.random.PRNGKey(int(time.time()))):
         super().__init__(N_CHANNELS, KERNEL_STR, ACTIVATION, PADDING, FIRE_RATE, KERNEL_SCALE, key)
         #key1,key2 = jax.random.split(key,2)
         key = jax.random.fold_in(key,1)
