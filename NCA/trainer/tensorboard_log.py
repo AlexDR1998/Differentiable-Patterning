c289b4c80401078082ecd6eeca28c731d9c9b998
Alex <alexander.richardson@gmail.com>
2025-04-16 17:31:51 +0100

    cleanup

--- NCA/trainer/tensorboard_log.py
+++ NCA/trainer/tensorboard_log.py
@@ -109,19 +109,7 @@ class aNCA_Train_log(NCA_Train_log):
 		#Log weights and biasses of model every 10 training epochs
 		
 		pass
-			# w1 = nca.layers[0].weight[:,:,0,0]
-			# w2 = nca.layers[2].weight[:,:,0,0]
-			# b2 = nca.layers[2].bias[:,0,0]
-			#w1,w2 = nca.get_weights()		
-			#self.log_histogram('Input layer weights',w1,step=i)
-			#self.log_histogram('Output layer weights',w2,step=i)
 			
-			#weight_matrix_figs = plot_weight_matrices(nca)
-			#self.log_image("Weight matrices",np.array(weight_matrix_figs)[:,0],step=i)
-					
-			#kernel_weight_figs = plot_weight_kernel_boxplot(nca)
-			#self.log_image("Input weights per kernel",np.array(kernel_weight_figs)[:,0],step=i)
-
 
 
 
@@ -131,20 +119,10 @@ class aNCA_Train_log(NCA_Train_log):
 class kaNCA_Train_log(NCA_Train_log):
 	def log_model_parameters(self,nca,i):
 		#Log weights and biasses of model every 10 training epochs
-		
-		# w1 = nca.layers[0].weight[:,:,0,0]
-		# w2 = nca.layers[2].weight[:,:,0,0]
-		# b2 = nca.layers[2].bias[:,0,0]
 		w1,w2 = nca.get_weights()		
 		self.log_histogram('Input layer weights',w1,step=i)
 		self.log_histogram('Output layer weights',w2,step=i)
 		
-		#weight_matrix_figs = plot_weight_matrices(nca)
-		#self.log_image("Weight matrices",np.array(weight_matrix_figs)[:,0],step=i)
-				
-		#kernel_weight_figs = plot_weight_kernel_boxplot(nca)
-		#self.log_image("Input weights per kernel",np.array(kernel_weight_figs)[:,0],step=i)
-
 
 
 class kaNCA_Train_pde_log(kaNCA_Train_log):
@@ -159,12 +137,7 @@ class mNCA_Train_log(NCA_Train_log):
 	def log_model_parameters(self,nca,i):
 		#Log weights and biasses of model every 10 training epochs
 		
-		# w1 = nca.layers[0].weight[:,:,0,0]
-		# w2 = nca.layers[2].weight[:,:,0,0]
-		# b2 = nca.layers[2].bias[:,0,0]
 		for scale,W in enumerate(nca.get_weights()):
-			#self.log_histogram('Input layer weights',W,step=i)
-			#print(W)
 			w1,w2,b2 = W
 			w1 = np.squeeze(w1)
 			w2 = np.squeeze(w1)
