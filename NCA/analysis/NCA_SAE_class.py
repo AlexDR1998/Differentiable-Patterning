2ae5e3dab4b9abde2cd1c9de2c38e894efd0dd55
Alex Richardson <alexander.richardson@gmail.com>
2025-04-24 10:54:16 +0000

    cleaned up SAE

--- NCA/analysis/NCA_SAE_class.py
+++ NCA/analysis/NCA_SAE_class.py
@@ -24,8 +24,9 @@ class SparseAutoencoder(AbstractModel):
                  N_KERNELS: int,
                  ACTIVATION: str, 
                  GATED: bool, 
-                 hidden_dim: int, 
+                 latent_dim: int, 
                  sparsity_param: float, 
+                 TIED_INIT: bool,
                  key):
         keys = jr.split(key,3)
         assert TARGET_LAYER in ["perception", "linear_hidden", "activation", "linear_output", "gate_func"], "Invalid target layer"
@@ -44,16 +45,18 @@ class SparseAutoencoder(AbstractModel):
         elif TARGET_LAYER == "gate_func":
             input_dim = N_CHANNELS
         
-        self.encoder = eqx.nn.Linear(input_dim, hidden_dim, key=keys[0],use_bias=False)
-        self.decoder = eqx.nn.Linear(hidden_dim, input_dim, key=keys[1],use_bias=False)
+        self.encoder = eqx.nn.Linear(in_features=input_dim, out_features=latent_dim, key=keys[0],use_bias=False)
+        self.decoder = eqx.nn.Linear(in_features=latent_dim, out_features=input_dim, key=keys[1],use_bias=False)
         w_where = lambda l: l.weight
         self.bias_params = [
             jr.normal(keys[2],(input_dim,)),
-            jr.normal(keys[3],(hidden_dim,))
+            jr.normal(keys[3],(latent_dim,))
         ]
 
         #b_where = lambda l: l.bias
-        self.decoder = eqx.tree_at(w_where,self.decoder,self.encoder.weight.T+1e-3*jax.random.normal(keys[4],self.decoder.weight.shape))
+        if TIED_INIT:
+            self.decoder = eqx.tree_at(w_where,self.decoder,self.encoder.weight.T+1e-6*jax.random.normal(keys[4],self.decoder.weight.shape))
+        
         self.sparsity_param = sparsity_param
         self.config = {
             "TARGET_LAYER": TARGET_LAYER,
@@ -61,18 +64,18 @@ class SparseAutoencoder(AbstractModel):
             "N_KERNELS": N_KERNELS,
             "ACTIVATION": ACTIVATION,
             "GATED": GATED,
-            "hidden_dim": hidden_dim,
+            "latent_dim": latent_dim,
             "sparsity_param": sparsity_param
         }
 
-    def encode(self,x: Float[Array,"{self.N_CHANNELS}"])->Float[Array,"{self.hidden_dim}"]:
+    def encode(self,x: Float[Array,"{self.N_CHANNELS}"])->Float[Array,"{self.latent_dim}"]:
         activation = {"topk":self.topk_activation, "relu":jax.nn.relu, "identity":lambda x:x}[self.ACTIVATION]
         return activation(self.encoder(x-self.bias_params[0])+self.bias_params[1])
     
-    def decode(self,x: Float[Array,"{self.hidden_dim}"])->Float[Array,"{self.N_CHANNELS}"]:
+    def decode(self,x: Float[Array,"{self.latent_dim}"])->Float[Array,"{self.N_CHANNELS}"]:
         return self.decoder(x) + self.bias_params[0]
     
-    def topk_activation(self,x: Float[Array,"{self.hidden_dim}"])->Float[Array,"{self.hidden_dim}"]:
+    def topk_activation(self,x: Float[Array,"{self.latent_dim}"])->Float[Array,"{self.latent_dim}"]:
         vals,inds = jax.lax.top_k(x,k=self.sparsity_param)
         output = jnp.zeros_like(x)
         output = output.at[inds].set(vals)
@@ -80,37 +83,31 @@ class SparseAutoencoder(AbstractModel):
     
     @eqx.filter_jit
     def mult_kth_top_feature(self,
-                             latents: Float[Array,"{self.hidden_dim}"],
+                             latents: Float[Array,"{self.latent_dim}"],
                              K: Int,
-                             val: Float)->Float[Array,"{self.hidden_dim}"]:
+                             val: Float)->Float[Array,"{self.latent_dim}"]:
         # Multiples kth top latent feature by val
         K = K+1
         vals,inds = jax.lax.top_k(latents,k=self.sparsity_param)
         inds_2 = jnp.argpartition(vals,-K,axis=0)[-K]
         pos = inds[inds_2]
-        #pos = jnp.argpartition(latents,-K,axis=0)[-K]
         latents = latents.at[pos].set(latents[pos]*val)
         return latents
     @eqx.filter_jit
     def mult_k_top_features(self,
-                            latents: Float[Array,"{self.hidden_dim}"],
+                            latents: Float[Array,"{self.latent_dim}"],
                             Ks,
-                            mult)->Float[Array,"{self.hidden_dim}"]:
+                            mult)->Float[Array,"{self.latent_dim}"]:
         # Multiples k top latent features by vals
-        #print(Ks)
         vals,inds = jax.lax.top_k(latents,k=self.sparsity_param)
         inds_2 = jnp.argsort(vals,axis=0)[::-1][Ks]
-        #print(inds_2.shape)
         pos = inds[inds_2]
-        #print(pos.shape)
-        #print(latents.shape)
-        #print(latents[pos].shape)
         latents = latents.at[pos].set(latents[pos]*mult)
         return latents
 
     @eqx.filter_jit
     def get_top_k_feature_positions(self,
-                           latents: Float[Array,"{self.hidden_dim}"])->Float[Array,"{self.sparsity_param}"]:
+                           latents: Float[Array,"{self.latent_dim}"])->Float[Array,"{self.sparsity_param}"]:
         # Returns the k top latent feature positions, sorted by feature activation
         vals,inds = jax.lax.top_k(latents,k=self.sparsity_param)
         inds_2 = jnp.argsort(vals,axis=0)[::-1]
@@ -120,14 +117,19 @@ class SparseAutoencoder(AbstractModel):
     
     @eqx.filter_jit
     def set_features_at_positions(self,
-                                  latents: Float[Array,"{self.hidden_dim}"],
+                                  latents: Float[Array,"{self.latent_dim}"],
                                   positions: Float[Array,"L"],
-                                  values: Float[Array,"L"])->Float[Array,"{self.hidden_dim}"]:
+                                  values: Float[Array,"L"])->Float[Array,"{self.latent_dim}"]:
         # Sets the features at positions to values
         latents = latents.at[positions].set(values)
         return latents
     
-    
+    # def normalise_decoder(self):
+    #     # Normalise the decoder weights such that each row is of unit length
+    #     # This is done so the decoder doesn't shrink the outputs of the latent space to minimise the loss
+    #     new_weights = self.decoder.weight / jnp.linalg.norm(self.decoder.weight,axis=1,keepdims=True)
+    #     self.decoder = eqx.tree_at(lambda l: l.weight,self.decoder,new_weights)
+        
     
     @eqx.filter_jit
     def __call__(self, x):
