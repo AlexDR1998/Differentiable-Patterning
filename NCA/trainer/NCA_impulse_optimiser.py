81c122002cdbb0ec0a516787970e382f901a8709
Alex Richardson <alexander.richardson@gmail.com>
2025-10-22 10:26:49 +0000

    WIP optimise impulses to switch between configurations

--- NCA/trainer/NCA_impulse_optimiser.py
+++ NCA/trainer/NCA_impulse_optimiser.py
@@ -22,17 +22,19 @@ class NCA_impulse_optimiser(object):
     """
         This class takes a pre-trained NCA and finds localised perturbations (impulses) that trigger the NCA to switch between its stable configurations.
     """
-    def __init__(self,
-                 NCA_model,
-                 data,
-                 DATA_AUGMENTER,
-                 STEPS_TO_STABLE,
-                 FILENAME,
-                 BOUNDARY_MASK = None,
-                 BOUNDARY_MODE = "soft",
-				 OBS_CHANNELS = None,
-                 LOG_DIRECTORY ="logs/",
-                 MODEL_DIRECTORY = "models/"):
+    def __init__(
+            self,
+            NCA_model,
+            data,
+            DATA_AUGMENTER,
+            STEPS_TO_STABLE,
+            FILENAME,
+            BOUNDARY_MASK = None,
+            BOUNDARY_MODE = "soft",
+            OBS_CHANNELS = None,
+            LOG_DIRECTORY ="logs/",
+            MODEL_DIRECTORY = "models/"
+        ):
         
         self.NCA_model = NCA_model
         self.CHANNELS = self.NCA_model.N_CHANNELS
@@ -105,5 +107,101 @@ class NCA_impulse_optimiser(object):
         return np.stack(final_states)
 
 
+    def loss_func(self,x,y):
+        """
+            Handles observable channel selection for loss calculation.
+            Parameters
+            ----------
+            x : float32 array [BATCHES,CHANNELS,WIDTH,HEIGHT]
+                Predicted states from NCA
+            y : float32 array [BATCHES,CHANNELS,WIDTH,HEIGHT]
+                Target stable configurations
+            Returns
+            -------
+            loss_value : float32 [BATCHES]
+                Loss value reduced over channel and spatial axes
+        """
+        x_obs = x[:,:self.OBS_CHANNELS]
+        y_obs = y[:,:self.OBS_CHANNELS]
+        return self._loss_func(x_obs,y_obs,key=jr.PRNGKey(0),where=None)
 
 
+    def train(
+            self,
+            iters,
+            optimiser = optax.adam(1e-3),
+            log_interval = 100,
+            LOSS_FUNC_STR = "l2",
+
+        ):
+        LOSS_FUNCS = {
+			"l2":loss.l2,
+			"l1":loss.l1,
+			"vgg":loss.vgg_hyperspectral,#lambda x,y,key,where:loss.vgg_hyperspectral(x,y,key,where,experiment_groups=LOSS_ARGS["experiment_groups"]),
+			"vgg_3ch":loss.vgg,
+			"euclidean":loss.euclidean,
+			"spectral":loss.spectral,
+			"spectral_full":loss.spectral_weighted,
+			# "rand_euclidean":lambda x,y,key:loss.random_sampled_euclidean(x,y,key=key)
+		}
+        self.OPTIMISER = optimiser
+        self._loss_func = LOSS_FUNCS[LOSS_FUNC_STR]
+        @eqx.filter_jit
+        def makestep(dx,nca,x,y,t,opt_state,key):
+            """
+            Single optimisation step to find impulse dx that drives NCA from x to y in t steps
+            We want to find dx such that NCA^t(x+dx) -> y 
+            Parameters
+            ----------
+            dx : float32 array [BATCHES,CHANNELS,WIDTH,HEIGHT]
+                Current impulse perturbation to be optimised
+            nca : NCA model
+                The NCA model to be optimised
+            x : float32 array [BATCHES,CHANNELS,WIDTH,HEIGHT]
+                Stable configuration initial conditions
+            y : float32 array [BATCHES,CHANNELS,WIDTH,HEIGHT]
+                Stable configuration target states
+            t : int
+                Number of steps to run NCA for
+            opt_state : optax optimiser state
+                Current optimiser state
+            key : jr.PRNGKey
+                Jax random key
+            Returns
+            -------
+            dx : float32 array [BATCHES,CHANNELS,WIDTH,HEIGHT]
+                Updated impulse perturbation
+            opt_state : optax optimiser state
+                Updated optimiser state
+            loss_value : float32
+                Current loss value
+            """
+
+            @eqx.filter_value_and_grad
+            def compute_loss(dx,nca,x,y,t,key):
+                v_nca = jax.vmap(nca,in_axes=(0,None,0),out_axes=0,axis_name="B")
+                def nca_step(carry,j):
+                    key,x = carry
+                    key = jr.fold_in(key,j)
+                    keys = jr.split(key,len(x))
+                    x = v_nca(x,lambda x:x,keys)
+                    # x = v_nca(x, self.BOUNDARY_CALLBACK, jr.split(key,len(x)))
+                    return (key,x),None
+                # Apply impulse at first step only
+                x = x + dx
+                (key,x),_ = eqx.internal.scan(nca_step,(key,x),xs=np.arange(t),kind="lax")
+                loss_batches = self.loss_func(x,y)
+                loss_value = np.mean(loss_batches)
+                return loss_value
+            loss,grads = compute_loss(dx,nca,x,y,t,key)
+            updates, opt_state = self.OPTIMISER.update(grads, opt_state, dx)
+            dx = eqx.apply_updates(dx, updates)
+            return dx, opt_state, loss
+
+
+
+        dx = np.zeros_like(self.data[0]) # Initialise impulse perturbation to zero
+        opt_state = self.OPTIMISER.init(dx)
+
+        # pbar = tqdm(range(iters))
+        # for i in pbar:
