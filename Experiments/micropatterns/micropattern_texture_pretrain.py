0dccaef4d5e3ea43eb6c07ee044d5da689f7ea2f
Alex Richardson <alexander.richardson@gmail.com>
2025-11-13 12:23:36 +0000

    varying noise strength in data augmenter

--- Experiments/micropatterns/micropattern_texture_pretrain.py
+++ Experiments/micropatterns/micropattern_texture_pretrain.py
@@ -41,19 +41,21 @@ TRAINING_ITERATIONS = 10000
 
 FULL_HYPERPARAMETERS = {
     # "model":["mNCA","gNCA","NCA"],
-    "loss_mode":["l2","vgg","vgg_grouped","vgg_and_l2","vgg_grouped_and_l2"],
-    "model":["NCA",],
+    "loss_mode":["vgg","vgg_grouped","vgg_and_l2","vgg_grouped_and_l2"],
+    "model":["NCA","gNCA"],
     "optimizer":["nadam"],
     "block_norm":[True],
+    # "noise_strength":[0.001,0.01,0.1],
+    "noise_strength":[0.1],
     "multistep":[1],
-    "intermediate_growth":[0.0,1.0],
+    "channels":[32],
+    "intermediate_growth":[0.0,0.2,1.0,2.0],
     "contiguous_growth":[0.0],
     "downsample":[8],
-    "channels":[32],
     "grad_loss": [False]
 }
 
-HPARAMS = index_to_param_list(index,5,FULL_HYPERPARAMETERS)
+HPARAMS = index_to_param_list(index,4,FULL_HYPERPARAMETERS)
 
 
 def load_data(DOWNSAMPLE,GROUPED):
@@ -79,6 +81,7 @@ def load_data(DOWNSAMPLE,GROUPED):
         DATA_CHANNELS = 8
     return data, aux,CHANNEL_NAMES,boundary_mask,augmenter,DATA_CHANNELS
 
+
 def run_training(H,key):
     key = jr.fold_in(key,index) 
     MODEL = H["model"]
@@ -94,6 +97,7 @@ def run_training(H,key):
     LOSS_MODE = H["loss_mode"]
     GRAD_LOSS = H["grad_loss"]
     DOWNSAMPLE = H["downsample"]
+    NOISE_STRENGTH = H["noise_strength"]
     INTERMEDIATE_GROWTH_COEFF = H["intermediate_growth"]
     CONTIGUOUS_GROWTH_COEFF = H["contiguous_growth"]
     STEPS_BETWEEN_IMAGES = int(256 / np.sqrt(DOWNSAMPLE))
@@ -118,6 +122,26 @@ def run_training(H,key):
         "individual": DataAugmenterBasic,
         "grouped_colony": DataAugmenterGrouped,
     }[augmenter]
+
+    @eqx.filter_jit
+    def jittable_callback_bit(x,x_true,OBS_CHANNELS):
+        propagate_xn = lambda x:x.at[1:].set(x[:-1])
+        reset_x0 = lambda x,x_true:x.at[0].set(x_true[0])
+        x = jax.tree_util.tree_map(propagate_xn,x) # Set initial condition at each X[n] at next iteration to be final state from X[n-1] of this iteration
+        x = jax.tree_util.tree_map(reset_x0,x,x_true) # Keep first initial x correct
+        for b in range(len(x)//2):
+            x[b*2] = x[b*2].at[:,:OBS_CHANNELS].set(x_true[b*2][:,:OBS_CHANNELS]) # Set every other batch of intermediate initial conditions to correct initial conditions
+        return x
+    
+    class DA_subclass(DA):
+        def data_callback(self,x,y,i,key):
+            x_true,_ =self.split_x_y(1)	
+            x = jittable_callback_bit(x,x_true,self.OBS_CHANNELS)
+            x = self.noise(x,NOISE_STRENGTH,key=key)
+            #y = self.noise(y,0.01,key=jax.random.fold_in(key,2*i))
+            self.PREVIOUS_KEY = key
+            return x,y
+        
     print("Data shape = " + str(data.shape))
     print("Boundary mask shape = " + str(boundary_mask.shape))
     warmup_steps = 100  # number of steps for warmup
@@ -155,19 +179,19 @@ def run_training(H,key):
     print("-----------------------------------------------------------------------------------------------------")
     nca = model(**NCA_hyperparameters)
     print(f"Training {nca.get_config()['MODEL']} on with STEPS_BETWEEN_IMAGES: {STEPS_BETWEEN_IMAGES} CHANNELS: {CHANNELS}")
-    if INTERMEDIATE_GROWTH_COEFF ==1.0:
-        reg_str = "_int"
-    else:
-        reg_str = ""
-    if CONTIGUOUS_GROWTH_COEFF ==1.0:
-        reg_str += "_contig"
-    
-    FILENAME = f"micropattern_circle_8ch_3colony_individual_{LOSS_MODE}_{opt_str}{reg_str}_{nca.get_config()['MODEL']}_t{STEPS_BETWEEN_IMAGES}_ch{CHANNELS}_ds{DOWNSAMPLE}_48h_stable"
+    # if INTERMEDIATE_GROWTH_COEFF ==:
+        # reg_str = "_int"
+    # else:
+    #     reg_str = ""
+    # if CONTIGUOUS_GROWTH_COEFF ==1.0:
+    #     reg_str += "_contig"
+
+    FILENAME = f"micropattern_circle_8ch_3colony_individual_{LOSS_MODE}_{opt_str}_int{INTERMEDIATE_GROWTH_COEFF}_contig_{CONTIGUOUS_GROWTH_COEFF}_noise{NOISE_STRENGTH}_{nca.get_config()['MODEL']}_t{STEPS_BETWEEN_IMAGES}_ch{CHANNELS}_ds{DOWNSAMPLE}_48h_stable"
     opt = NCA_Trainer(
         nca,
         data,
         model_filename=FILENAME,
-        DATA_AUGMENTER=DA,
+        DATA_AUGMENTER=DA_subclass,
         MODEL_DIRECTORY=PVC_PATH+"models/",
         LOG_DIRECTORY=PVC_PATH+"logs/",
         OBS_CHANNELS=OBS_CHANNELS,
@@ -192,30 +216,31 @@ def run_training(H,key):
         # loss_str = ["vgg","l2"]
     else:
         raise ValueError("Invalid LOSS_MODE")
-    try:
-        opt.train(
-            t=STEPS_BETWEEN_IMAGES,
-            iters=TRAINING_ITERATIONS,
-            REGULARISER_COEFFS={
-                "intermediate_state":INTERMEDIATE_GROWTH_COEFF,
-                "boundary": 1.0,
-                "contiguous_growth":CONTIGUOUS_GROWTH_COEFF,
-            },
-            WARMUP=warmup_steps,
-            optimiser=optimiser,
-            WRITE_IMAGES=True,
-            LOSS_FUNC_STR=loss_str,
-            wandb_args={
-                "project":"nca-micropatterns",
-                "group":"ind_8ch_3colony_loss_channel_opt_48h_v2",
-                "tags":["training",nca.get_config()['MODEL'],str(CHANNELS)+"ch",str(DOWNSAMPLE)+"x_downsample",LOSS_MODE],
-                "name":FILENAME
-            },
-            LOG_EVERY=100,
-            CLEAR_CACHE_EVERY=500,
-        )
-    except Exception as e:
-        print(f"Training failed with hyperparameters {H} exception: {e}")
+    # try:
+    opt.train(
+        t=STEPS_BETWEEN_IMAGES,
+        iters=TRAINING_ITERATIONS,
+        REGULARISER_COEFFS={
+            "intermediate_state":INTERMEDIATE_GROWTH_COEFF,
+            "boundary": 1.0,
+            "contiguous_growth":CONTIGUOUS_GROWTH_COEFF,
+        },
+        WARMUP=warmup_steps,
+        optimiser=optimiser,
+        WRITE_IMAGES=True,
+        LOSS_FUNC_STR=loss_str,
+        wandb_args={
+            "project":"nca-micropatterns",
+            "group":"ind_8ch_3colony_loss_channel_opt_48h_stable_intreg",
+            # "tags":["training",nca.get_config()['MODEL'],str(CHANNELS)+"ch",str(DOWNSAMPLE)+"x_downsample",LOSS_MODE],
+            "tags":[f"{k}:{v}" for k,v in H.items()],
+            "name":FILENAME
+        },
+        LOG_EVERY=100,
+        CLEAR_CACHE_EVERY=500,
+    )
+    # except Exception as e:
+        # print(f"Training failed with hyperparameters {H} exception: {e}")
     return key
 
 
