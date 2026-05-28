578a85137072faae599a5d0b2fa2caeccfce8914
Alex <alexander.richardson@gmail.com>
2026-05-28 17:12:01 +0100

    setting up envsubst

---
+++ exports_and_envsubst.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <arg1> <arg2>"
+  exit 1
+fi
+
+ARG1="$1"
+ARG2="$2"
+
+export WORKING_DIR=/user/$USER 
+export JOB_PATH_TO_PYTHON_SCRIPT=$ARG1
+export JOB_COMPLETION_INDEX=$ARG2
+envsubst < run.tpl.yml > run_test.yml
