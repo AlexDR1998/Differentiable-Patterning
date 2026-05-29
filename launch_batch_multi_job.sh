2598977c2d1e30436ccec2b3a96151affcbb77d5
Alex <alexander.richardson@gmail.com>
2026-05-29 15:02:05 +0100

    handling uneven division of experiment configs to jobs

--- launch_batch_multi_job.sh
+++ launch_batch_multi_job.sh
@@ -9,6 +9,11 @@ FILE="$1"
 CONFIGS="$2"
 JOB_WORKER_COUNT="$3"
 
+TOTAL_CONFIGS=$(grep -c '^-[[:space:]]*index:' "$CONFIGS")
+BASE_PODS=$((TOTAL_CONFIGS / JOB_WORKER_COUNT))
+REMAINDER=$((TOTAL_CONFIGS % JOB_WORKER_COUNT))
+
+
 
 export WORKING_DIR=/user/$USER/Differentiable-Patterning/
 export PATH_TO_PYTHON_SCRIPT=$FILE
@@ -18,6 +23,15 @@ export JOB_WORKER_COUNT=$JOB_WORKER_COUNT
 
 for ((i=0; i<JOB_WORKER_COUNT; i++)); do
   export JOB_WORKER_INDEX=$i
+  if (( i < REMAINDER )); then
+    export N_PODS=$((BASE_PODS + 1))
+  else
+    export N_PODS=$BASE_PODS
+  fi
+  if (( N_PODS == 0 )); then
+    echo "Skipping worker $i because there are no configs left for it"
+    continue
+  fi
   envsubst < run.tpl.yml > run_$i.yml
   kubectl create -f run_$i.yml
 done
