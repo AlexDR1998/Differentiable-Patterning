59ff54163f35aa268ee2b4da14ad33635fd08ee4
Alex <alexander.richardson@gmail.com>
2026-06-01 12:05:10 +0100

    fixing paths

--- launch_inside_pod.sh
+++ launch_inside_pod.sh
@@ -5,7 +5,22 @@ if [ $# -lt 2 ]; then
   exit 1
 fi
 
-# FILE="$1"
-# CONFIGS="$2"
-# python $FILE $CONFIGS
-python $1 --manifest $2
+normalize_workspace_path() {
+  local input_path="$1"
+  case "$input_path" in
+    /*)
+      printf '%s\n' "$input_path"
+      ;;
+    workspace/*)
+      printf '/%s\n' "$input_path"
+      ;;
+    *)
+      printf '/workspace/%s\n' "$input_path"
+      ;;
+  esac
+}
+
+SCRIPT_PATH="$(normalize_workspace_path "$1")"
+MANIFEST_PATH="$(normalize_workspace_path "$2")"
+
+python "$SCRIPT_PATH" --manifest "$MANIFEST_PATH"
