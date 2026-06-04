d274c1c1899418c75971a519b085518771780cc9
Alex <alexander.richardson@gmail.com>
2026-06-04 14:50:17 +0100

    added CLI option for quickly selecting GPU types

--- launch_batch_multi_job.sh
+++ launch_batch_multi_job.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-if [ $# -lt 3 ]; then
-  echo "Usage: $0 <path_to_python_script> <path_to_experiment_config> <number_of_workers>"
+if [ $# -lt 4 ]; then
+  echo "Usage: $0 <path_to_python_script> <path_to_experiment_config> <number_of_workers> <gpu_type>"
   exit 1
 fi
 
 FILE="$1"
 CONFIGS="$2"
 JOB_WORKER_COUNT="$3"
+GPU_TYPE="${4:-H200}"
+
+if [ "$GPU_TYPE" == "H200" ]; then
+  GPU_MEMORY=140
+  GPU_FULL_NAME="NVIDIA-H200"
+elif [ "$GPU_TYPE" == "A100" ]; then
+  GPU_MEMORY=80
+  GPU_FULL_NAME="NVIDIA-A100-SXM4-80GB"
+elif [ "$GPU_TYPE" == "H100" ]; then
+  GPU_MEMORY=80
+  GPU_FULL_NAME="NVIDIA-H100-80GB-HBM3"
+else
+  echo "Unknown GPU type: $GPU_TYPE"
+  exit 1
+fi
 
 TOTAL_CONFIGS=$(grep -c '^-[[:space:]]*index:' "$CONFIGS")
 BASE_PODS=$((TOTAL_CONFIGS / JOB_WORKER_COUNT))
@@ -20,6 +35,8 @@ export PATH_TO_PYTHON_SCRIPT=$FILE
 # export COMPLETION_INDEX=$CONFIGS
 export PATH_TO_EXPERIMENT_CONFIG=$CONFIGS
 export JOB_WORKER_COUNT=$JOB_WORKER_COUNT
+export GPU_TYPE=$GPU_FULL_NAME
+export GPU_MEM_LIMIT="${GPU_MEMORY}Gi"
 
 for ((i=0; i<JOB_WORKER_COUNT; i++)); do
   export JOB_WORKER_INDEX=$i
