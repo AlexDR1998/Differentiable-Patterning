7762ecaaf256efd77d98a428821de228f75aaccc
Alex <alexander.richardson@gmail.com>
2026-07-16 11:27:39 +0100

    testing runtime of sycl NCA implementation

--- Experiments/config_helpers.py
+++ Experiments/config_helpers.py
@@ -9,6 +9,7 @@ from NCA.model.NCA_gated_model import gNCA
 from NCA.model.NCA_gated_noise_model import gnNCA
 from NCA.model.NCA_model import NCA
 from NCA.model.NCA_model_fast import NCA as NCAFast
+from NCA.model.NCA_sycl import NCA as NCASycl
 from NCA.model.NCA_noise_model import nNCA
 from NCA.model.NCA_upsample_isotropic_model import uNCA as isouNCA
 from NCA.model.NCA_upsample_model import uNCA
@@ -338,6 +339,16 @@ def build_model(cfg, key=None):
         )
     elif cfg.model.family == "NCA_fast":
         model = NCAFast(
+            N_CHANNELS=cfg.model.channels,
+            KERNEL_STR=cfg.model.kernel_str,
+            ACTIVATION=activation,
+            FIRE_RATE=cfg.model.fire_rate,
+            PADDING=cfg.model.padding,
+            KERNEL_SCALE=kernel_scale,
+            key=key,
+        )
+    elif cfg.model.family == "NCA_sycl":
+        model = NCASycl(
             N_CHANNELS=cfg.model.channels,
             KERNEL_STR=cfg.model.kernel_str,
             ACTIVATION=activation,
